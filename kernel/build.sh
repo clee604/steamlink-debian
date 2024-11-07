03a574e61c8354d3138dd3e2ca34f65fc9b9eb99
djmuted <jaero13@gmail.com>
2024-11-07 15:54:35 +0100

    Run kernel build script

---
+++ kernel/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir boot
+cd steamlink-sdk
+source setenv.sh
+cd ../linux-$KERNEL_VERSION
+make oldconfig
+make -j$(nproc)
+make modules
+mkdir /tmp/build-modules
+INSTALL_MOD_PATH=/tmp/build-modules make modules_install
