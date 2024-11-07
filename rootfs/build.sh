8b3e1b73c0634a073bcd9de7ebf73aa4757ef082
djmuted <jaero13@gmail.com>
2024-11-07 22:34:17 +0100

    Add build rootfs script

--- rootfs/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker build . --platform linux/arm/v7 --rm --tag deblink
-docker rm deblink || true
-docker create --platform linux/arm/v7 -t -i --name deblink deblink
-
-docker export deblink -o deblink.tar
