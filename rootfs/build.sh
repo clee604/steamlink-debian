8c4b94d3ab7df653b16b195d1e24fdd43751f948
djmuted <jaero13@gmail.com>
2024-11-08 02:11:23 +0100

    Remove env vars from image build script

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -2,17 +2,16 @@
 
 set -e
 
-IMG_FILE="steamlink-debian-$DEBIAN_VERSION-$KERNEL_VERSION.img"
-dd if=/dev/zero of=$IMG_FILE bs=1M count=1024
+dd if=/dev/zero of=steamlink-debian.img bs=1M count=1024
 
 # Use parted to create a partition table and a single primary partition
-parted --script $IMG_FILE mklabel msdos        # Create an msdos partition table
-parted --script $IMG_FILE mkpart primary ext3 1MiB 100%  # Create a primary partition
+parted --script steamlink-debian.img mklabel msdos        # Create an msdos partition table
+parted --script steamlink-debian.img mkpart primary ext3 1MiB 100%  # Create a primary partition
 
 sync
 
 # Set up a loop device with partition mapping
-LOOP_DEV=$(losetup --show -P -f $IMG_FILE)
+LOOP_DEV=$(losetup --show -P -f steamlink-debian.img)
 
 sync
 
@@ -31,4 +30,4 @@ umount -l /mnt/disk
 losetup -d $LOOP_DEV
 
 # Make the image file readable for non-root users
-chmod a+r $IMG_FILE
+chmod a+r steamlink-debian.img
